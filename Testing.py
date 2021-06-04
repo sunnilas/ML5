cfa0dfcef0e9dbadafb0213ea61f9d55ec84d752
Sunil Kumar <sunil@Sunils-MacBook-Pro.local>
2021-06-04 20:48:34 +0530

    ML5

--- Testing.py
+++ Testing.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
+
 st.write("""
 # SVM implementation app
  """)
-esting#Text Input
-age = st.text_input("Enter the Age",)
+# Text Input
+age = st.text_input("Enter the Age", )
 salary = st.text_input("Enter the Salary: (Salary in Rupees)")
-gender =  st.text_input("Enter the Gender: (1-Male,0-Female)")
+gender = st.text_input("Enter the Gender: (1-Male,0-Female)")
 submit = st.button("Classify")
